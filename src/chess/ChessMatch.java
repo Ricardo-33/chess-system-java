848fc68c923a9fb0b4a92c9ad281713d5f65531f
Ricardo-33 <edermask21@gmail.com>
2021-12-01 16:52:44 -0300

    Placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,6 +11,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	// Retorna uma matriz com as peças de xadrez correspondente a partida	
@@ -19,5 +23,11 @@ public class ChessMatch {
 			}
 		}
 		return (mat);
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 7));
+		board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
 	}
 }
